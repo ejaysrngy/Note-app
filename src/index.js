854a7ee08829f55ca8cb03b22cdb3d68b5c2fd37
ejaysrngy <ejaysarangay@gmail.com>
2021-12-14 14:10:08 +0800

    simplified code

--- src/index.js
+++ src/index.js
@@ -4,21 +4,26 @@ import {Header, Footer} from "./App";
 import Note from "./Note";
 import notes from "./notes";
 
-function createNote(props){
-  return (
-    <Note
-      key={props.key}
-      title={props.title}
-      content={props.content}
-      />
-  )
-};
+// function createNote{
+//   return (
+//     <Note
+//       key={props.key}
+//       title={props.title}
+//       content={props.content}
+//       />
+//   )
+// };
 
 ReactDOM.render(
   <div>
       <Header />
       <div className="note-container container-flex span12 col-lg col-md">
-          {notes.map(createNote)}
+          {notes.map((props) => 
+          <Note
+            key={props.key}
+            title={props.title}
+            content={props.content}
+            /> )}
       </div>
       <Footer />
   </div>
