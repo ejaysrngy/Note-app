9fc6ffce9f56bff743653b3a65689f787e71b824
ejaysrngy <ejaysarangay@gmail.com>
2021-12-21 13:30:08 +0800

    add icons

--- src/Note.jsx
+++ src/Note.jsx
@@ -6,6 +6,10 @@ function Note(props) {
         <div className="note">
             <h1> {props.title} </h1>
             <p> {props.content} </p>
+            <div className="icons">
+                <i className="fi fi-rr-edit"></i>
+                <i className="fi fi-rr-trash"></i>
+            </div>
         </div>
     )
 }
